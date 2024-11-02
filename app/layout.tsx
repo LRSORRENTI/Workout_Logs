51c95eb469de784d0193dab937cad0b831547d42
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-11-02 13:23:26 -0500

    added more styling changes to toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,44 @@
+// import './globals.css';
+// import type { Metadata } from 'next';
+// import { Inter } from 'next/font/google';
+// import { Toaster } from "@/components/ui/toaster";
+// import { ThemeProvider } from "../components/theme-providers";
+// const inter = Inter({ subsets: ['latin'] });
+
+// export const metadata: Metadata = {
+//   title: 'Workout Tracker',
+//   description: 'Track your daily workouts and exercises',
+// };
+
+// export default function RootLayout({
+//     children,
+//   }: {
+//     children: React.ReactNode;
+//   }) {
+//     return (
+//       <html lang="en" suppressHydrationWarning>
+//         <head />
+//         <body>
+//           <ThemeProvider
+//             attribute="class"
+//             defaultTheme="system"
+//             enableSystem
+//             disableTransitionOnChange
+//           >
+//             {children}
+//           </ThemeProvider>
+//         </body>
+//       </html>
+//     );
+//   }
+// app/layout.tsx
+
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "../components/theme-providers";
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -11,23 +47,24 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    );
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster /> {/* Include the Toaster component here */}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
