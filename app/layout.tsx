8477226bed73c0b299d517d80a2ca4add259e8d2
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-11-02 10:20:34 -0500

    Added dark mode toggle from shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from "@/components/ui/toaster";
-
+import { ThemeProvider } from "../components/theme-providers";
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -11,16 +11,23 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Toaster />
-      </body>
-    </html>
-  );
-}
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    );
+  }
